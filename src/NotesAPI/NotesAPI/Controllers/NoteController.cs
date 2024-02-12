ccb875ccdab333af66671a9bb5ab99a9196e04ca
Julian Emilio Osorio Larroche <julianosoriolarroche@gmail.com>
2024-02-12 08:30:17 -0500

    Note Controller was added
    Note Controller was added.

---
+++ src/NotesAPI/NotesAPI/Controllers/NoteController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotesAPI.Models;
+
+namespace NotesAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class NoteController : ControllerBase
+  {
+    public static List<Note> notes = new List<Note> {
+      new Note {
+        Id = 1,
+        Author = "Julian",
+        Date = DateTime.Now,
+        Content = "Tomar agua" },
+      new Note {
+        Id = 2,
+        Author = "violeta",
+        Date = DateTime.Now,
+        Content = "jums" }
+
+      };
+
+    [HttpGet]
+    public async Task<ActionResult<List<Note>>> GetAllNotes()
+    {
+      return Ok(notes);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<List<Note>>> GetSingleNote(int id)
+    {
+      var note = notes.Find(x => x.Id == id);
+      return Ok(note);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<List<Note>>> GetSingleNote(Note note)
+    {
+      notes.Add(note);
+      return Ok(note);
+    }
+  }
+}
---
+++ src/NotesAPI/NotesAPI/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace NotesAPI.Models
+{
+  public class Note
+  {
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public string Author { get; set; } = string.Empty;
+   }
+}
